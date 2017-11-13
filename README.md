13f16f3a858f3a1c92440caeb1ae15db39c306ba
Erick Merchant <erickmerchant@gmail.com>
2017-11-12 19:30:40 -0800

    Remove 4 unused plugins

--- README.md
+++ README.md
@@ -9,13 +9,9 @@ __for css__
 - [postcss](http://postcss.org/)
 - [postcss-import](https://github.com/postcss/postcss-import#readme)
 - [postcss-font-family-system-ui](https://github.com/JLHwung/postcss-font-family-system-ui)
-- [postcss-pseudo-class-any-link](https://github.com/jonathantneal/postcss-pseudo-class-any-link)
-- [postcss-custom-selectors](https://github.com/postcss/postcss-custom-selectors)
 - [postcss-custom-properties](https://github.com/postcss/postcss-custom-properties)
 - [postcss-custom-media](https://github.com/postcss/postcss-custom-media)
 - [postcss-media-minmax](https://github.com/postcss/postcss-media-minmax)
-- [postcss-color-gray](https://github.com/postcss/postcss-color-gray)
-- [postcss-color-function](https://github.com/postcss/postcss-color-function)
 - [autoprefixer](https://github.com/postcss/autoprefixer)
 - [cssnano](http://cssnano.co/)
 
--- src/styles/plugins.js
+++ src/styles/plugins.js
@@ -1,12 +1,9 @@
 const cssimport = require('postcss-import')
 const systemFont = require('postcss-font-family-system-ui')
-const anyLink = require('postcss-pseudo-class-any-link')
-const customSelectors = require('postcss-custom-selectors')
 const customProperties = require('postcss-custom-properties')
 const customMedia = require('postcss-custom-media')
 const mediaMinMax = require('postcss-media-minmax')
-const colorGray = require('postcss-color-gray')
-const colorFunction = require('postcss-color-function')
+
 const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
 
@@ -14,13 +11,9 @@ module.exports = function (args) {
   const plugins = [
     cssimport(),
     systemFont(),
-    anyLink(),
-    customSelectors(),
     customProperties(),
     customMedia(),
     mediaMinMax(),
-    colorGray(),
-    colorFunction(),
     autoprefixer({browsers: args.browsers})
   ]
 
