0c19cbda9452c0351340072415e7ec8fc108c1cf
Erick Merchant <erickmerchant@gmail.com>
2017-12-21 03:11:43 -0800

    Work on new tests

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
 const path = require('path')
-const thenify = require('thenify')
-const mkdirp = thenify(require('mkdirp'))
-const chokidar = require('chokidar')
 
-module.exports = function (types) {
-  return function (args) {
-    return mkdirp(args.destination).then(function () {
-      return Promise.all(Object.keys(types).map(function (ext) {
-        const config = {
-          input: args[ext],
-          output: path.join(args.destination, 'bundle.' + ext)
-        }
-
-        const run = types[ext](args, config)
-
-        if (args.watch) {
-          chokidar.watch(path.join(path.dirname(args[ext]), '**/*.' + ext), {ignoreInitial: true}).on('all', function () {
-            return run()
-          })
-        }
-
-        return run()
-      }))
+module.exports = function (deps) {
+  return function ({option, parameter}) {
+    option('css', {
+      description: 'where is your css',
+      default: { value: './css/index.css' }
     })
+
+    option('js', {
+      description: 'where is your js',
+      default: { value: './js/index.js' }
+    })
+
+    parameter('destination', {
+      description: 'where to save to',
+      default: { value: '.' }
+    })
+
+    option('no-min', {
+      description: 'do not minify',
+      type: Boolean
+    })
+
+    option('electron', {
+      description: 'build for electron',
+      type: Boolean
+    })
+
+    option('watch', {
+      description: 'watch for changes',
+      type: Boolean,
+      aliases: ['w']
+    })
+
+    option('browsers', {
+      description: 'what browsers to target',
+      default: { value: ['last 2 versions', '> 5%'] },
+      multiple: true
+    })
+
+    return function (args) {
+      return deps.makeDir(args.destination).then(function () {
+        return Promise.all(Object.keys(deps.types).map(function (ext) {
+          const config = {
+            input: args[ext],
+            output: path.join(args.destination, 'bundle.' + ext)
+          }
+
+          return deps.watch(args.watch, path.join(path.dirname(args[ext]), '**/*.' + ext), deps.types[ext](args, config))
+        }))
+      })
+    }
   }
 }
---
+++ test.js
@@ -0,0 +1,81 @@
+const test = require('tape')
+// const chalk = require('chalk')
+const execa = require('execa')
+const stream = require('stream')
+const out = new stream.Writable()
+
+out._write = () => { }
+
+const noopDeps = {
+  makeDir () {},
+  watch () {},
+  types: {}
+}
+// const noopDefiners = {
+//   parameter: () => {},
+//   option: () => {}
+// }
+
+test('index.js - options and parameters', function (t) {
+  t.plan(16)
+
+  const parameters = {}
+  const options = {}
+
+  require('./index')(noopDeps)({
+    parameter: (name, args) => {
+      parameters[name] = args
+    },
+    option: (name, args) => {
+      options[name] = args
+    }
+  })
+
+  t.ok(options.css)
+
+  t.equal(options.css.default.value, './css/index.css')
+
+  t.ok(options.js)
+
+  t.equal(options.js.default.value, './js/index.js')
+
+  t.ok(parameters.destination)
+
+  t.equal(parameters.destination.default.value, '.')
+
+  t.ok(options['no-min'])
+
+  t.equal(options['no-min'].type, Boolean)
+
+  t.ok(options.electron)
+
+  t.equal(options.electron.type, Boolean)
+
+  t.ok(options.watch)
+
+  t.equal(options.watch.type, Boolean)
+
+  t.deepEqual(options.watch.aliases, ['w'])
+
+  t.ok(options.browsers)
+
+  t.equal(options.browsers.multiple, true)
+
+  t.deepEqual(options.browsers.default.value, ['last 2 versions', '> 5%'])
+})
+
+test('cli.js', async function (t) {
+  t.plan(4)
+
+  try {
+    await execa('node', ['./cli.js', '-h'])
+  } catch (e) {
+    t.ok(e)
+
+    t.equal(e.stderr.includes('Usage'), true)
+
+    t.equal(e.stderr.includes('Options'), true)
+
+    t.equal(e.stderr.includes('Parameters'), true)
+  }
+})
